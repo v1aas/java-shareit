fe13bf6043b0a557616d3b4521ff63d9b7fd1b06
Vlas <dron.2001sv@mail.ru>
2023-08-13 01:50:12 +0300

    <fix><Подогнать под чек-стайл>

--- server/src/main/java/ru.practicum.shareit/user/controller/UserController.java
+++ server/src/main/java/ru.practicum.shareit/user/controller/UserController.java
@@ -10,7 +10,6 @@ import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.service.UserService;
 
 import javax.persistence.EntityNotFoundException;
-import javax.validation.Valid;
 import java.util.List;
 
 @RestController
